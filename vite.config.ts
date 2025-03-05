a9167df970adcedd492984da7790c36ce815a70f
Vicky Rodriguez <vrod8763@gmail.com>
2025-03-04 19:37:08 -0500

    build: Configure Terser minification for production builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Lighthouse performace score went from 90 to 89 after adding minify - perhaps it's worth it to remove? 
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn'],
+        passes: 2,
+      },
+      mangle: {
+        toplevel: true,
+      },
+      format: {
+        comments: false,
+      },
+    }
+  }
 })
